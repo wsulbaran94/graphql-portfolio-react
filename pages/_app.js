5b2a5de4b919a7b44819eb0e62c5f99c77c440a3
Wilfredo Sulbaran <wilfredosulbaran94@gmail.com>
2022-02-16 11:30:29 -0500

    initial props from app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import App from 'next/app'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/index.scss';
@@ -9,12 +10,19 @@ const MyApp = ({Component, pageProps}) => {
   return (
     <div className="portfolio-app">
       <Navbar/>
+      { pageProps.appData }
       {Component.name === 'Home' && <Hero/>}
       <div className='container'>
         <Component {...pageProps}/>
       </div>
     </div>
   )
+}
+
+MyApp.getInitialProps = async (context) => {
+  const initialProps = App.getInitialProps && await App.getInitialProps(context);
+
+  return { pageProps: {...initialProps, appData: 'just is to text App_'}}
 }
 
 export default MyApp;
